b31d63b9e69d8da755eea493e5063950048144e4
Sosuke Aizen <verrandyb@gmail.com>
2023-11-09 00:24:16 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,115 @@
-import Image from 'next/image'
+import Image from "next/image"
+import {
+  Icon
+} from '@iconify-icon/react'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+import {
+  BsInstagram,
+  BsWhatsapp
+} from 'react-icons/bs';
+
+import {
+  TbNoCopyright
+} from 'react-icons/tb';
+
+export default function HomePage() {
+    return (
+      <div className="">
+        <Image
+          src={"/images/bali.jpg"}
+          alt="backoground-bali"
+          fill={true}
+          className="fixed top-0 -z-10  left-0 w-screen h-screen object-cover"
+        />
+
+        <div className="lg:mt-12 mt-8  z-10 lg:flex lg:px-0 px-6 lg:justify-center">
+          <div className="lg:w-2/5 bg-white px-4 pb-16 rounded-xl w-full">
+            <div className="logo w-full">
+              <div className="w-full   flex justify-center">
+                <img
+                  src={"/images/1.png"}
+                  className="lg:w-64 w-32 mt-4 lg:mt-6   h-auto object-cover"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="description mt-2">
+              <div className="text-center">
+                <p className="lg:text-lg text-sm font-semibold">
+                  We provide services for your Holiday in Bali.
+                </p>
+                <p className="lg:text-sm text-gray-600 mt-2 text-xs font semibold">
+                  Providing the perfect home at the best value in bali.
+                </p>
+              </div>
+            </div>
+            <div className="mt-6 flex justify-center">
+              <h1 className="font-semibold text-base lg:text-xl lg:mb-4 ">
+                Our Social Media
+              </h1>
+            </div>
+            <div className=" Social Media">
+              <ul className="flex flex-col lg:items-center">
+                <li className="my-2 lg:w-2/3">
+                  <a
+                    href="https://www.instagram.com/bali.tourntravel/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA=="
+                    className="bg-white items-center space-x-4  w-full lg:px-20 justify-center py-3 transition-all duration-500 hover:border-red-600 flex hover:bg-red-600 hover:text-white rounded-md border border-gray-400"
+                  >
+                    <BsInstagram />
+                    <span>Instagram</span>
+                  </a>
+                </li>
+                {/* <li className="my-2 lg:w-2/3 ">
+                  <button className="bg-white w-full lg:px-20 justify-center py-3 transition-all duration-500 hover:border-red-600 flex hover:bg-red-600 hover:text-white rounded-md border border-gray-400">
+                    Instagram
+                  </button>
+                </li> */}
+              </ul>
+            </div>
+
+            <div className="Admin Contacts lg:mt-6 mt-4">
+              <h1 className="text-center lg:text-lg font-semibold text-sm">
+                Admin Contacts
+              </h1>
+              <ul className="flex flex-col lg:items-center">
+                <li className="my-2 lg:w-2/3">
+                  <a
+                    href="https://api.whatsapp.com/send?phone=6282211558215"
+                    className="bg-white items-center space-x-4  w-full lg:px-20 justify-center py-3 transition-all duration-500 hover:border-green-600 flex hover:bg-green-600 hover:text-white rounded-md border"
+                  >
+                    <BsWhatsapp />
+                    <span>Admin 1</span>
+                  </a>
+                </li>
+                <li className="my-2 lg:w-2/3">
+                  <a
+                    href="https://api.whatsapp.com/send?phone=6281238163603"
+                    className="bg-white items-center space-x-4  w-full lg:px-20 justify-center py-3 transition-all duration-500 hover:border-green-600 flex hover:bg-green-600 hover:text-white rounded-md border"
+                  >
+                    <BsWhatsapp />
+                    <span>Admin 2</span>
+                  </a>
+                </li>
+                {/* <li className="my-2 lg:w-2/3 ">
+                  <button className="bg-white w-full lg:px-20 justify-center py-3 transition-all duration-500 hover:border-red-600 flex hover:bg-red-600 hover:text-white rounded-md border border-gray-400">
+                    Instagram
+                  </button>
+                </li> */}
+              </ul>
+            </div>
+
+            <div className="lg:mt-24 mt-16 flex items-center space-x-2 justify-center">
+              <div className="flex items-center">
+                <span>Copyrights</span>
+                <TbNoCopyright />
+              </div>
+              <h1 className="text-center font-bold text-sm">
+                Tour n Travel Bali
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
+
